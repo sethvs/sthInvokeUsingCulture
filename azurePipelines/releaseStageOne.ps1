63124bcdf68c4700c5ad79cbdde3a77857181e78
Sergey Vasin <sethvs@users.noreply.github.com>
2018-11-15 19:53:56 +0300

    Azure Pipelines Releases. (#6)

---
+++ azurePipelines/releaseStageOne.ps1
@@ -0,0 +1,12 @@
+$ModuleName = 'sthInvokeUsingCulture'
+
+$SourcePath = Join-Path -Path $(System.DefaultWorkingDirectory) -ChildPath "_$ModuleName"
+$ModulePath = Join-Path -Path $(System.DefaultWorkingDirectory) -ChildPath $ModuleName
+
+New-Item -ItemType Directory -Path $ModulePath | Out-Null
+Copy-Item -Path $SourcePath\* -Include *.psd1, *.psm1, *.ps1,*.ps1xml -Exclude *_* -Destination $ModulePath
+Copy-Item -Path $SourcePath\Tests -Destination $ModulePath -Exclude *_* -Recurse
+Copy-Item -Path $SourcePath\en-us -Destination $ModulePath -Exclude *_* -Recurse
+Copy-Item -Path $SourcePath\ru-ru -Destination $ModulePath -Exclude *_* -Recurse
+
+Get-ChildItem $ModulePath -Recurse
